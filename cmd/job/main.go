110a0a528d024e2461829c18f130b01c0d365a1e
kong <suphanat.bo@inet.co.th>
2024-11-25 11:37:49 +0700

    add http

--- cmd/job/main.go
+++ cmd/job/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	// err := godotenv.Load()
 	// if err != nil {
@@ -7,4 +12,18 @@ func main() {
 	// }
 
 	createScheduler()
+
+	http.HandleFunc("/", helloHandler)
+
+	// Start the server on port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
 }
